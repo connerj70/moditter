d2355d19778883fe90af2df3ce95d24f9d83c83c
Conner Jensen <connerj70@gmail.com>
2021-03-14 10:36:58 -0600

    A

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/about', to: 'about#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#register'
   get '/register', to: 'users#register'
