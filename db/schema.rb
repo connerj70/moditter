b4728b338c69260dbdbbb1616b1e7200db687805
Conner Jensen <connerj70@gmail.com>
2021-05-16 16:23:31 -0600

    schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_003752) do
+ActiveRecord::Schema.define(version: 2021_01_23_220851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,20 +19,15 @@ ActiveRecord::Schema.define(version: 2021_01_29_003752) do
     t.string "oauth_token"
     t.string "oauth_token_secret"
     t.string "screen_name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "oauth_user_id"
     t.index ["user_id"], name: "index_oauths_on_user_id"
   end
 
-  create_table "testers", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tweet_views", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.integer "count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
